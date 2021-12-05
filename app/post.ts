c0c4c95acad4469ed4c4ded393ee03c0b52faef4
mohannadhanafi <mohannad.hanafi96@gmail.com>
2021-12-05 13:35:50 +0200

    Feature: get the posts from files.

--- app/post.ts
+++ app/post.ts
@@ -1,18 +1,39 @@
+import path from "path";
+import fs from "fs/promises";
+import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
+
 export type Post = {
   title: string;
   slug: string;
 };
 
-export const getPosts = () => {
-  const posts: Post[] = [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
-  return posts;
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
+
+const postsPath = path.join(__dirname, "..", "posts");
+
+export const getPosts = async () => {
+  const dir = await fs.readdir(postsPath);
+  return Promise.all(
+    dir.map(async (filename) => {
+      const file = await fs.readFile(path.join(postsPath, filename));
+      const { attributes } = parseFrontMatter(file.toString());
+      invariant(
+        isValidPostAttributes(attributes),
+        `${filename} has bad meta data!`
+      );
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title,
+      };
+    })
+  );
 };
